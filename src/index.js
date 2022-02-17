1d77a8e954759843fb973c3b1de5bb4a5a6f666f
SonaShahgeldyan <sona.shahgeldyan28@gmail.com>
2022-02-17 20:02:57 +0400

    fix(index): add error handeling

--- src/index.js
+++ src/index.js
@@ -1,41 +1,36 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const findFiles = (dir) => {
-  fs.readdir(dir, (err, files) => {
-    if (err) {
-      return err;
-    } else {
-      files.forEach((file) => {
-        const filePath = dir + "/" + file;
-        fs.lstat(filePath, (err, stats) => {
-          if (err) {
-            return err;
+  return fs.readdir(dir).then((files) => {
+    files.forEach((file) => {
+      const filePath = dir + "/" + file;
+      fs.lstat(filePath, (err, stats) => {
+        if (err) {
+          return err;
+        } else {
+          if (stats.isDirectory()) {
+            findFiles(filePath);
           } else {
-            if (stats.isDirectory()) {
-              findFiles(filePath);
-            } else {
-              const fileInfo = filePath + "-------" + stats.size + "kb" + "\n";
-              fs.writeFile(
-                "./src/sorted_files.txt",
-                fileInfo,
-                { flag: "a" },
-                (err) => {
-                  if (err) {
-                    return err;
-                  }
+            const fileInfo = filePath + "-------" + stats.size + "kb" + "\n";
+            fs.writeFile(
+              "./src/sorted_files.txt",
+              fileInfo,
+              { flag: "a" },
+              (err) => {
+                if (err) {
+                  return err;
                 }
-              );
-            }
+              }
+            );
           }
-        });
+        }
       });
-    }
+    });
   });
 };
 
-// const currentDir = __dirname;
-
 const args = process.argv.slice(2)[0];
-console.log(args);
 
-findFiles(args);
+findFiles(args).catch((err) => {
+  console.log(err.message);
+});
